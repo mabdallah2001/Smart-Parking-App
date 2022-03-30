e26bff5f641ab6615bd03b7b100c6f8e7240a083
Mohammed <b00079469@aus.edu>
2022-03-30 19:40:33 +0400

    Fifth Commit: Fines

--- screens/FinesScreen.js
+++ screens/FinesScreen.js
@@ -1,46 +1,102 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet, View, Image, ScrollView } from 'react-native'
 import { Text } from 'react-native-elements'
 import { Card, Title, Paragraph } from 'react-native-paper';
+import {db} from '../firebase';
+import axios from 'axios';
 
 
 
 const FinesScreen = () => {
-    const [fine, setFine] = useState(true)
+    const [fine, setFine] = useState(false)
+    const [owner, setOwner] = useState("");
+
+    const [tickNo, setTickNo] = useState();
+    const [amount, setAmount] = useState();
+    const [provider, setProvider] = useState();
+    const [loc, setLoc] = useState();
+    const [reason, setReason] = useState();
+    const [date, setDate] = useState();
+    const [time, setTime] = useState();
+    const [render, rerender] = useState(false);
+
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/receive-key").then(function(response){
+          setOwner(response.data);
+        });
     
+        db.collection('Fines').where('Owner', '==', owner).get().then(snapshot => {
+            snapshot.forEach(doc => {
+                setTickNo(doc.get('Ticket Number'));
+                setAmount(doc.get('Amount'));
+                setProvider(doc.get('Provider'));
+                setLoc(doc.get('Location'));
+                setReason(doc.get('Reason'));
+                setDate(doc.get('Date'));
+            });
+        })
+        .catch(err => {
+            console.log('Error getting documents', err);}
+        );
+
+
+        // db.collection('Fines').onSnapshot(querySnapshot => {
+        //     querySnapshot.docChanges().forEach(change => {
+        //         setTickNo(change.doc.get('Ticket Number'));
+        //         setAmount(change.doc.get('Amount'));
+        //         setProvider(change.doc.get('Provider'));
+        //         setLoc(change.doc.get('Location'));
+        //         setReason(change.doc.get('Reason'));
+        //         setDate(change.doc.get('Date'));
+
+        //     });
+        // });
+
+        if(tickNo === undefined){
+            setFine(false)
+        }
+        else{
+            setFine(true);
+        }
+
+        setTimeout(() => {
+            rerender(!render);
+        }, 10000);
+
+
+    },[render]);
+
     return (
         fine?
         <ScrollView>
             <Card style={styles.card}>
                 <Card.Content>
-
-
-
                     
                     <Image style={{resizeMode:'contain', height:'10%', width:'10%'}} source={require('../icons/AUS.jpg')}></Image>
                     
                     <Text style={{fontSize:10, alignSelf:'flex-end', top:-18}}>18/01/2022 ∘ 10:48 AM</Text>
                     <View style={{flexDirection:'row', flexWrap:'wrap' , marginTop: 15}}>
                         <Image style={styles.icon} source={require('../icons/person.png')}></Image>
-                        <Text>AUS Security</Text>
+                        <Text>{provider}</Text>
                     </View>
                     <View style={{flexDirection:'row', flexWrap:'wrap' , marginTop: 10}}>
                         <Image style={styles.icon} source={require('../icons/location.png')}></Image>
-                        <Text>Sports Complex</Text>
+                        <Text>{loc}</Text>
                         
                     </View>
                     <View style={{flexDirection:'row', flexWrap:'wrap', marginTop: 10}}>
                         <Image style={styles.icon} source={require('../icons/ticket.png')}></Image>
-                        <Text>Parking in a paid parking lot</Text>
+                        <Text>{reason}</Text>
                     </View>
                     <View style={{flexDirection:'row', flexWrap:'wrap', marginTop: 30, alignSelf:'center'}}>
                     <View>
-                    <Text style={{alignSelf:'center'}}>78987328</Text>
+                    <Text style={{alignSelf:'center'}}>{tickNo}</Text>
                     <Text style={{color:'#909090'}}>Ticket Number</Text>
                     </View>
                     <View style={styles.verticleLine}></View>
                     <View>
-                    <Text style={{alignSelf:'center'}}>50 AED</Text>
+                    <Text style={{alignSelf:'center'}}>{amount} AED</Text>
                     <Text style={{color:'#909090'}}>Total Amount</Text>
                     </View>
                     </View>
